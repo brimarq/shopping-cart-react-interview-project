6ba84280fe025c312de191e5811a9007e85fe49f
Brian Marquis <rbmarquis.dev@gmail.com>
2024-04-26 15:43:48 -0500

    access store from cart page to get total price from added items

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,3 +1,14 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 export default function Cart() {
+  const [totalCart, setTotalCart] = useState(0);
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, cur) => acc + cur.price, 0));
+  }, [cart]);
+
+  console.log(cart, totalCart);
   return <div>Cart</div>;
 }
